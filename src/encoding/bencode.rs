c2d248f2daceda905831fbe501539ef6c0f31003
VanillaBrooks <brookskarlik@gmail.com>
2019-05-06 22:10:45 -0700

    added preliminary decoding bencoded info

--- src/encoding/bencode.rs
+++ src/encoding/bencode.rs
@@ -1,4 +1,5 @@
-extern crate hashbrown;
+use hashbrown;
+use regex::Regex;
 pub mod percent {
     pub fn encode(){
 
@@ -6,7 +7,6 @@ pub mod percent {
 }
 
 // TODO: preallocation of strings based on input size
-#[macro_use]
 
 #[derive(Debug)]
 pub enum Bencoding {
@@ -77,3 +77,90 @@ macro_rules! bencode {
     };
 }
 
+pub enum DeBencode {
+    Num(i32),
+    Str(String),
+    Dic(hashbrown::HashMap<String,String>),
+    Lis(Vec<String>)
+}
+#[derive(Debug)]
+pub enum FlatBencode{ 
+    Num,
+    Str,
+    Dic,
+    Lis,
+    Empty
+}
+
+fn check_letter(input: &str, character: &str) -> bool {
+    match input.get(0..1) {
+        Some(x) => {
+            if x == character{
+                return true
+            }
+            return false
+        }
+        None => return false
+    }
+
+}
+
+pub fn get_next_stop(input_str: &str) -> FlatBencode {
+    let re = Regex::new(r"[dli]-*\d|\d[:]").unwrap();
+    let k = re.find(input_str).unwrap();
+    let start = k.start();
+    dbg!{k};
+    match input_str.get(start..start+1){
+        Some("d") => FlatBencode::Dic,
+        Some("l") => FlatBencode::Lis,
+        Some("i") => FlatBencode::Num,
+        Some(_) => FlatBencode::Str,
+        None => FlatBencode::Empty
+    }
+}
+
+// D<contents>E
+pub fn dic(input: &str) -> Result<DeBencode, &str> {
+    if !check_letter(&input, "d"){return Err("parsing dictionary without correct letter starts")}
+
+    let content_to_check = input.get(1..input.len()).unwrap();
+    
+    let end_dict = Regex::new(r"[e]&&\d{1}|[i]&&\d+|-{1}").unwrap();
+
+    let num = "sampleei34e";
+    let stri  = "";
+    let list = "";
+    let dic = "";
+
+    return Ok(DeBencode::Num(23))
+    
+
+}
+// // L<contents>E
+// fn lis(input: &str) -> Result<DeBencode, String> {
+//     return Result<DeBencode::Num(23)>
+// }
+// // I<num>E
+// fn num(input: &str) ->Result<DeBencode, String> {
+//     return Result<DeBencode::Num(23)>
+// }
+// //bytecount : string
+// fn stri(input: &str) ->Result<DeBencode, String>{
+//     return Result<DeBencode::Num(23)>
+// }
+
+
+
+
+
+
+
+
+
+// TEST CASES ENCODING
+	// dbg!(bencode!{"one_word"});
+	// dbg!(bencode!{["sequence", "of", "values"]});
+	// dbg!(bencode!{{"sample":"dictionary","multiple":"values"}});
+	// dbg!(bencode!{raw: "raw_one_word"});
+	// dbg!(bencode!{raw: 234i32});
+	// println!{"{:?}", 23i32.bencode()}
