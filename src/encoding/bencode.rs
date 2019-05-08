dbe635790414d96c66e236f73a1a335f4a4ad666
VanillaBrooks <brookskarlik@gmail.com>
2019-05-08 01:26:52 -0700

    legacy decoding cases

--- src/encoding/bencode.rs
+++ src/encoding/bencode.rs
@@ -1,5 +1,7 @@
 use hashbrown;
 use regex::Regex;
+use std::cmp::Ordering;
+
 pub mod percent {
     pub fn encode(){
 
@@ -47,7 +49,7 @@ impl Bencode for &str {
 }
 
 #[macro_export]
-macro_rules! bencode {
+macro_rules! ben_encode {
     ([$($element:tt),*]) =>{
         $crate::encoding::bencode::Bencoding::Lis(
             vec![
@@ -92,6 +94,20 @@ pub enum FlatBencode{
     Empty
 }
 
+struct Decoding_Positions {
+    pub ty: FlatBencode,
+    pub start: u32,
+    pub end: u32,
+    pub contained_str: String
+}
+
+impl Decoding_Positions{
+    fn new(ty: FlatBencode, start:u32, end:u32, contained_str: String)->Decoding_Positions{
+        return Decoding_Positions{ty:ty, start:start, end:end, contained_str: contained_str}
+    }
+}
+
+
 fn check_letter(input: &str, character: &str) -> bool {
     match input.get(0..1) {
         Some(x) => {
@@ -105,32 +121,150 @@ fn check_letter(input: &str, character: &str) -> bool {
 
 }
 
-pub fn get_next_stop(input_str: &str) -> FlatBencode {
-    let re = Regex::new(r"[dli]-*\d|\d[:]").unwrap();
-    let k = re.find(input_str).unwrap();
-    let start = k.start();
-    dbg!{k};
-    match input_str.get(start..start+1){
-        Some("d") => FlatBencode::Dic,
-        Some("l") => FlatBencode::Lis,
-        Some("i") => FlatBencode::Num,
-        Some(_) => FlatBencode::Str,
-        None => FlatBencode::Empty
+
+
+pub fn general(input: &str) -> Result<DeBencode, &str> {
+
+	let dict_start = Regex::new(r"d[l\di]").unwrap();
+
+	let lis_start = Regex::new(r"l[idl\d]").unwrap();
+	let collection_end = Regex::new(r"ee|\d:[[:alpha:]]+e^[i\dd]").unwrap();
+
+    // let dict_start = dict_start.find(&input);
+    // let dict_end = dict_end.find(&input);
+    
+    // match 
+    let dict_starts: Vec<_>= dict_start.find_iter(&input).map(|x| (x.start()+1) as i32).collect();
+    let collection_ends: Vec<_> = collection_end.find_iter(&input).map(|x| (x.end()-1) as i32).collect();
+    let list_starts: Vec<_> = lis_start.find_iter(&input).map(|x| (x.start()+1) as i32).collect();
+
+    if dict_starts.len() + list_starts.len() != collection_ends.len() {
+        panic!{"number of dictionary starts ({:?}) did not equal dictionary ends ({:?}) for phrase {}",dict_starts, collection_ends, input}
     }
+
+    println!{"all dicts {:?}", dict_starts}
+    println!{"all lists {:?}" ,list_starts}
+    println!{"all of collections: {:?}", collection_ends}
+
+    // this may be error prone
+    let primary_iter = &list_starts;
+    let secondary_iter = &dict_starts;
+    let order = FlatBencode::Lis;
+    if dict_starts.len() > list_starts.len(){
+        println!{"primary is dict"}
+        let primary_iter = &dict_starts;
+        let secondary_iter = &list_starts;
+        let order = FlatBencode::Dic;
+    }
+    else{
+        println!{"primary is lists"}
+    }
+    
+    // TODO: Preallocate these 
+    let mut final_prim: Vec<(i32, i32)> = Vec::new();
+    let mut final_sec: Vec<(i32, i32)> = Vec::new();
+
+    let mut prim_i = 0;
+    let mut sec_i = 0;
+
+    for i in 0..collection_ends.len(){
+        println!{"\n\n"}
+        // println!{"here"}
+
+        let decision_prim = prim_i.cmp(&primary_iter.len());
+        let prim = match decision_prim{
+            (Ordering::Less) => primary_iter[prim_i],
+            _ => -1i32
+        };
+
+        let decision_sec = sec_i.cmp(&secondary_iter.len());
+        let sec = match decision_sec {
+            (Ordering::Less) => secondary_iter[sec_i],
+            _ => -1i32,
+        };
+        println!{"made it this far"}
+        let ender = collection_ends[prim_i+sec_i];
+        println!{"over ehre"}
+
+        if sec_i < secondary_iter.len(){
+            println!{"assigning a new value to sec"}
+            let sec =  secondary_iter[sec_i];
+        }
+        else{
+            println!{"we are keeping sec as 0 {} {}", sec_i, secondary_iter.len()}
+        }
+
+        println!{"primary is {} secondary is {} ender is {}", prim, sec, ender}
+
+        if (ender > sec)&& (sec> prim) {
+            final_sec.push((sec,ender));
+            sec_i +=1
+        }
+        else if (ender > prim) && (prim > sec) {
+            final_prim.push((prim,ender));
+            prim_i +=1;
+        }
+        panic!{"conditions not met for bencode :::{}:::", input}
+
+        // // if the distacne between the end and the secondary is smaller 
+        // if sec < 0{
+        //     final_prim.push((prim,ender));
+        //     prim_i +=1;
+        // }
+        // else if prim < 0{
+        //     final_sec.push((sec, ender));
+        //     sec_i+=1
+        // }
+        // else if (ender-sec) <0 {
+        //     println!{"first condition"}
+        //     final_prim.push((prim,ender));
+        //     prim_i +=1;
+        // }
+        // else if (ender-prim) <0 {
+        //     println!{"second condition {}", ender-prim}
+        //     final_sec.push((sec, ender));
+        //     sec_i+=1
+        // }
+        // else if (ender-prim) < (ender-sec){
+        //     println!{"3rd condition"}
+        //     final_prim.push((prim, ender));
+        //     prim_i +=1;
+        // }
+        // else if (ender-sec) < (ender-prim){
+        //     println!{"4th condition"}
+        //     final_sec.push((prim, ender));
+        //     sec_i+=1;
+        // }
+        // else {panic!{"conditions not met for bencode :::{}:::", input}}
+
+    println!{"END OF LOOP:"}
+    println!{"primary {:?}", final_prim}
+    println!{"secondary {:?}", final_sec}
+    }
+    
+
+    // let num_start = Regex::new(r"i[\-\d]").unwrap();
+    // let num_end = Regex::new(r"\de").unwrap();
+
+    // let str_start = Regex::new(r"\d:").unwrap();
+
+    // let lis_start = Regex::new(r"l[idl\d]").unwrap();
+	// let lis_end = Regex::new(r"ee|\d:[[:alpha:]]+e^[i\dd]").unwrap();
+
+    // let dict_starts = 
+
+    
+
+
+    
+    return Ok(DeBencode::Num(23))
+
 }
 
 // D<contents>E
 pub fn dic(input: &str) -> Result<DeBencode, &str> {
-    if !check_letter(&input, "d"){return Err("parsing dictionary without correct letter starts")}
 
-    let content_to_check = input.get(1..input.len()).unwrap();
-    
-    let end_dict = Regex::new(r"[e]&&\d{1}|[i]&&\d+|-{1}").unwrap();
 
-    let num = "sampleei34e";
-    let stri  = "";
-    let list = "";
-    let dic = "";
 
     return Ok(DeBencode::Num(23))
     
@@ -149,7 +283,29 @@ pub fn dic(input: &str) -> Result<DeBencode, &str> {
 //     return Result<DeBencode::Num(23)>
 // }
 
+#[macro_export]
+macro_rules! ben_decode {
+    ($input:expr) => {
+        let vec : Vec<DeBencode> = Vec::new();
+        let positions = general(input);
 
+        for i in general($x){
+            match i.ty{
+                FlatBencode::Num =>{
+                    ben_decode!(num: x, input.get(i.start..i.end))
+                }
+                FlatBencode::Str =>{}
+                FlatBencode::Dic=>{}
+                FlatBencode::Lis=> {}
+                FlatBencode::Empty =>{}
+
+                
+            }
+        } 
+    };
+    (num: $start:expr, $end:expr) =>{}
+
+}
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use hashbrown;
 use encoding::bencode::{Bencode};
 #[macro_use]
 mod encoding;
+use regex::Regex;
 
 
 
@@ -11,25 +12,25 @@ fn main() {
 	// let k :i32= regex::Regex::new("234").unwrap();
 
 	// let sample = ":spami42eed3:bar4:spam3:fooi42ee";
-	let digit = "i-42e";
-	let list = "l4:spami42ee";
-	let dict = "d3:bar4:spam3:fooi42ee";
-	let strin = "4:spam";
-	// let a = vec![
-	// get_next_stop(&digit),
-	// get_next_stop(&list),
-	// get_next_stop(&dict),
-	// get_next_stop(&strin),
-	// ];
-	// for i in a{
-	// 	dbg!{i};
-	// }
-	dbg!{bencode![[1,2,3,4,6]]};
+	// let digit = "i-42e";
+	// let list = "l4:spami42ee";
+	let dict = "d3:bar4:spam3:fooi42ei-42el4:spami42eei34ee";
+	// let strin = "4:spam";
+	// let num = "l4:spami-42ee";
 
-	    if true || true {
-        println!{"here"}
-    }
-	
-	// encoding::bencode::dic("dsample");
+	let lis_start = Regex::new(r"l[idl\d]").unwrap();
+	let lis_end = Regex::new(r"ee|\d:[[:alpha:]]+e^[i\dd]").unwrap();
+	let dict_end = Regex::new(r"d[l\di]").unwrap();
+
+	let starts: Vec<_> = lis_start.find_iter(&dict).map(|x| x.start()+1).collect();
+	let ends :Vec<_>= lis_end.find_iter(&dict).map(|x| x.end()-1).collect();
+	let dict_ends : Vec<_> = dict_end.find_iter(&dict).map(|x| x.end()-1).collect();
+
+	// println!{"dict starts: {:?} list starts: {:?} ends: {:?}", dict_ends, starts, ends}
+
+	encoding::bencode::general(&dict);
+	println!{"{}", dict.get(1..42).unwrap()}
+	dbg!{dict.get(27..37)};
+
 }
 
