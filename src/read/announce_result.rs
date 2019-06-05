868b8046f8880751744ceb0383bd462015faf0b0
VanillaBrooks <brookskarlik@gmail.com>
2019-06-05 05:13:21 -0700

    commit before rebase

--- src/read/announce_result.rs
+++ src/read/announce_result.rs
@@ -8,8 +8,21 @@ use std::io::prelude::*;
 use super::super::error::*;
 
 
-#[derive(Debug, Deserialize)]
 pub struct AnnounceResult {
+    pub info_hash: String,
+    pub announce_url: String,
+    pub data: AnnounceData
+}
+impl AnnounceResult{
+    pub fn new_bytes(input_bytes: &Vec<u8>, hash: String, url: String) -> Result<AnnounceResult, Error>{
+        let data = AnnounceData::new_bytes(&input_bytes)?;
+        let s = AnnounceResult{info_hash: hash, announce_url: url, data: data};
+        Ok(s)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AnnounceData  {
     pub complete: i64,
     pub incomplete: i64,
     pub downloaded: i64, 
@@ -17,18 +30,20 @@ pub struct AnnounceResult {
     #[serde(default)]
     pub peers: Option<ByteBuf>,
     #[serde(default)]
-    pub peers6: Option<ByteBuf>
+    pub peers6: Option<ByteBuf>,
+    // #[serde(default)]
+    // info_hash: Option<&'a str>
 }
 
-impl AnnounceResult {
-    pub fn new_bytes(input_bytes: &Vec<u8>) ->Result<AnnounceResult, Error> {
-        let ann = de::from_bytes::<AnnounceResult>(&input_bytes)?;
+impl AnnounceData {
+    pub fn new_bytes(input_bytes: &Vec<u8>) ->Result<AnnounceData, Error> {
+        let ann = de::from_bytes::<AnnounceData>(&input_bytes)?;
         Ok(ann)
     }
-    pub fn new_file(filename: &str) -> Result<AnnounceResult, Error> {
-        let mut buffer = Vec::new();
-        let mut file = std::fs::File::open(filename)?;
-        file.read_to_end(&mut buffer)?;
-        AnnounceResult::new_bytes(&buffer)
-    }
+    // pub fn new_file(filename: &str) -> Result<AnnounceData, Error> {
+    //     let mut buffer = Vec::new();
+    //     let mut file = std::fs::File::open(filename)?;
+    //     file.read_to_end(&mut buffer)?;
+    //     AnnounceData::new_bytes(&buffer)
+    // }
 }
