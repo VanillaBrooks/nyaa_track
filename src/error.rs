c770c4e9c6ce5547bde7a77c36e1446f08acbbfe
brooks <45132001+VanillaBrooks@users.noreply.github.com>
2019-06-23 23:56:58 -0700

    error impl macro

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,43 @@
 use postgres;
 use http;
 
+// from_type: Type that will be converted away from
+// to_type: Destination enum that we are converting to
+// subtype: the path to the branch of to_type that from_type will be converted to
+#[macro_export]
+macro_rules! impl_from {
+    // catch to expand to a function-like enum
+    (empty: $from_type:ident, $to_type:ty, $subtype:expr) => {
+        impl_from!(full: $from_type, $to_type, $subtype, exp_empty)
+    };// will expand to call impl_from!(exp_empty: ...)  ^^^^^^^
+
+    // catch for C-like enums
+    ($from_type:ident, $to_type:ty, $subtype:expr) => {
+        impl_from!(full: $from_type, $to_type, $subtype, exp_full)
+    };// will expand to call impl_from!(exp_full: ...)   ^^^^^^^
+
+    // this branch will be called always. interior macro will expand 
+    // depending on if the implementing type expects a C-link enum or not
+    (full: $from_type:ident, $to_type:ty, $subtype:expr, $expansion:ident) => {
+        impl From<$from_type> for $to_type {
+            impl_from!($expansion: $from_type, $subtype);
+        }
+    };
+
+    //interior expansion function if it is a C-like enum
+    (exp_empty: $from_type:ident, $subtype:expr) => {
+        fn from(error: $from_type) -> Self {
+            $subtype
+        }
+    };
+    // interior expansion function if it is a function-link enum
+    (exp_full: $from_type:ident, $subtype:expr) => {
+        fn from(error: $from_type) -> Self {
+            $subtype(error)
+        }
+    };
+}
+
 #[derive(Debug)]
 pub enum Error{
 	IO(std::io::Error),
