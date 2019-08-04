ff9a3fec3b1433c374673750cc0b620622863059
unknown <brookskarlik@gmail.com>
2019-08-03 22:23:36 -0700

    better initialization of data. separation of queries from schema

---
+++ src/database/queries.sql
@@ -0,0 +1,53 @@
+-- $1 - name of error string
+-- $2 - info hash string
+-- $3 poll time
+-- 
+
+
+with
+	type_id_
+	as
+	(
+		select type_id
+		from error_types
+		where error_name = $1
+	),
+	info_id_
+	as
+	(
+		select id
+		from info
+		where info_hash = $2
+	)
+insert into error
+	(err_type, info_id, poll_time)
+VALUES
+	(
+		(select *from type_id_), 
+		(select *from info_id_), 
+		$3);
+
+		
+
+with type_id_ as ( select type_id from error_types where error_name = $1 ), info_id_ as ( select id from info where info_hash = $2 ) insert into error (err_type, info_id, poll_time) VALUES ( (select * from type_id_), (select * from info_id_), $3);
+
+
+
+
+-- total the number of errors for each hash (and get the last update of that hash)
+select info.info_hash, error.info_id, count(error.info_id), (select to_timestamp(max(poll_time)) from stats where stats_id=error.info_id)
+from error
+inner join info on info.id = error.info_id
+group by error.info_id, info.info_hash;
+
+
+
+
+--- fetch most recent updates for torrents by time stamp
+select info.title, (select to_timestamp(max(stats.poll_time))) from info
+inner join stats on stats.stats_id = info.id
+
+where info.title like '%Horrible%' and info.title like '%Kimetsu%'
+group by info.id;
+
+
